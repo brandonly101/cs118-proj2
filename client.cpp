2283638cf73e7c32956490134784c721a445c322
Daniel Ong <danielong@Daniels-MacBook-Pro.local>
2016-12-02 17:22:12 -0800

    Adding old code

--- client.cpp
+++ client.cpp
@@ -24,81 +24,200 @@ int main(int argv, char* argv[])
 	// prob need to do an if else statement for ip addr or port number;
 	ipAddr = argv[1];
 	portNumber = argv[2];
-
-
-	/*
-  // create a socket using TCP IP
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-  // allow others to reuse the address
-  int yes = 1;
-  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-    perror("setsockopt");
-    return 1;
-  }
-
-  // bind address to socket
-  struct sockaddr_in addr;
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(40000);     // short, network byte order
-  addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
-
-  if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-    perror("bind");
-    return 2;
-  }
-
-  // set socket to listen status
-  if (listen(sockfd, 1) == -1) {
-    perror("listen");
-    return 3;
-  }
-
-  // accept a new connection
-  struct sockaddr_in clientAddr;
-  socklen_t clientAddrSize = sizeof(clientAddr);
-  int clientSockfd = accept(sockfd, (struct sockaddr*)&clientAddr, &clientAddrSize);
-
-  if (clientSockfd == -1) {
-    perror("accept");
-    return 4;
-  }
-
-  char ipstr[INET_ADDRSTRLEN] = {'\0'};
-  inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
-  std::cout << "Accept a connection from: " << ipstr << ":" <<
-    ntohs(clientAddr.sin_port) << std::endl;
-
-  // read/write data from/into the connection
-  bool isEnd = false;
-  char buf[20] = {0};
-  std::stringstream ss;
-
-  while (!isEnd) {
-    memset(buf, '\0', sizeof(buf));
-
-    if (recv(clientSockfd, buf, 20, 0) == -1) {
-      perror("recv");
-      return 5;
-    }
-
-    ss << buf << std::endl;
-    std::cout << buf << std::endl;
-
-
-    if (send(clientSockfd, buf, 20, 0) == -1) {
-      perror("send");
-      return 6;
-    }
-
-    if (ss.str() == "close\n")
-      break;
-
-    ss.str("");
-  }
-
-  close(clientSockfd);
-*/
   return 0;
 }
+
+/*
+
+
+struct URLObject
+{
+    string host;
+    string portnum;
+    string path;
+};
+
+URLObject parseURL(string url);
+void sendRequest(int sockfd, string url);
+void receiveResponse(int sockfd);
+void getAddr(string host, string portnum, string& ip);
+string getFileContentType(string file);
+
+// Constants
+const size_t MAX_HTTP_MESSAGE_SIZE = 65536;
+
+int main(int argc, char* argv[])
+{
+    // Each other argument is a URL for different HTTP requests.
+    string urlTemp;
+    for (int i = 1; i < argc; i++)
+    {
+        // Read in the URL arguments into a URL object.
+        URLObject urlObj = parseURL(argv[i]);
+
+        // create a socket using TCP IP
+        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd == -1) {
+            perror("socket error");
+            return 1;
+        }
+
+        string ip = "";
+        getAddr(urlObj.host, urlObj.portnum, ip);
+
+        if (ip == "") { continue; }
+
+        struct sockaddr_in serverAddr;
+        int port = atoi(urlObj.portnum.c_str());
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(port);     // short, network byte order
+        serverAddr.sin_addr.s_addr = inet_addr(ip.c_str());
+        memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
+
+        // connect to the server
+        if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
+        {
+            perror("connect() error");
+            return 2;
+        }
+
+        struct sockaddr_in clientAddr;
+        socklen_t clientAddrLen = sizeof(clientAddr);
+        if (getsockname(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen) == -1)
+        {
+            perror("getsockname() error");
+            return 3;
+        }
+
+        char ipstr[INET_ADDRSTRLEN] = {'\0'};
+        inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
+        // cout << "Set up a connection from: " << ipstr << ":" <<
+        // ntohs(clientAddr.sin_port) << endl;
+
+        cout << "Sending a request ..." << endl;
+
+        // Create the HTTP request. For now, the only method is GET 1.0.
+        HTTPRequest req;
+        req.setMethod("GET");
+        req.setURL(urlObj.path);
+		//cout << getFileContentType(urlObj.path);
+        req.setVersion("1.0");
+        string encodedReq = req.encode();
+
+        // Send the HTTP request.
+        if (send(sockfd, encodedReq.c_str(), encodedReq.size(), 0) == -1)
+        {
+            perror("send() error");
+            return 4;
+        }
+
+        // send/receive data to/from connection
+        char buffer[MAX_HTTP_MESSAGE_SIZE];
+
+        memset(buffer, '\0', sizeof(buffer));
+
+        // Wait for and receive the HTTP response.
+        if (recv(sockfd, buffer, MAX_HTTP_MESSAGE_SIZE, 0) == -1)
+        {
+            perror("recv() error");
+            return 5;
+        }
+        cout << buffer << endl;
+        string header = string(buffer);
+
+        // Download the file...
+        HTTPResponse response;
+        response.decode(string(buffer));
+        string filename = "." + req.getURL();
+        int fd = open(filename.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 777);
+        if (write(fd, response.getBody().c_str(), response.getBody().length()) == -1)
+        {
+            perror("read() error");
+            return -1;
+        }
+        close(fd);
+
+        //receiveResponse(sockfd, buffer);
+        close(sockfd);
+    }
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Helper function implementations.
+////////////////////////////////////////////////////////////////////////////////
+URLObject parseURL(string url)
+{
+    cout << "URL TO BE PARSED IS " << url << endl;
+    // Clean the URL by removing the "http://".
+    if (url.substr(0, 7) == "http://") { url = url.substr(7); }
+
+    // Create the URL struct.
+    URLObject urlObject;
+
+    // Parse the URL and apply it to the URL object as appropriate.
+    size_t colon = url.find(":");
+    size_t slash = url.find("/");
+
+    string host = url;
+    string port = "80";
+    string path = "/";
+
+    if (colon != string::npos) {
+        host = url.substr(0, colon);
+        host = host =="localhost" ? "127.0.0.1" : host;
+
+        if (slash != string::npos) {
+            port = url.substr(colon + 1, slash - (colon + 1));
+        }
+    } else if (slash != string::npos) {
+        host = url.substr(0, slash);
+    }
+
+    if (slash != string::npos) {
+        path = url.substr(slash);
+    }
+
+    urlObject.path = path;
+    urlObject.host = host;
+    urlObject.portnum = port;
+
+    return urlObject;
+}
+
+void getAddr(string host, string portnum, string &ip)
+{
+    struct addrinfo* res;
+    struct addrinfo hints;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    int status = getaddrinfo(host.c_str(), portnum.c_str(), &hints, &res);
+    if (status != 0) {
+        cerr << "getaddrinfo error" << endl;
+        exit(2);
+    }
+
+    char ret[INET_ADDRSTRLEN] = {'\0'};
+    struct sockaddr_in* ip_struct = (struct sockaddr_in*) res[0].ai_addr;
+
+    inet_ntop(res[0].ai_family, &(ip_struct->sin_addr), ret, sizeof(ret));
+
+    ip = ret;
+    freeaddrinfo(res);
+}
+
+
+string responseMsg(string statusCode)
+{
+    if (statusCode == "200")
+        return "HTTP/1.0 200 OK\r\n";
+    if (statusCode == "400")
+        return "HTTP/1.0 400 Bad Request\r\n";
+    if (statusCode == "404")
+        return "HTTP/1.0 404 Not Found\r\n";
+    return "Invald or not supported status code\r\n."
+}
+*/
--- server.cpp
+++ server.cpp
@@ -23,80 +23,219 @@ int main(int argc, char* argv[])
 
   portNumber = argv[1];
   fileName = argv[2];
-
-/*
-  // create a socket using TCP IP
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-  // allow others to reuse the address
-  int yes = 1;
-  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-    perror("setsockopt");
-    return 1;
-  }
-
-  // bind address to socket
-  struct sockaddr_in addr;
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(40000);     // short, network byte order
-  addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
-
-  if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-    perror("bind");
-    return 2;
-  }
-
-  // set socket to listen status
-  if (listen(sockfd, 1) == -1) {
-    perror("listen");
-    return 3;
-  }
-
-  // accept a new connection
-  struct sockaddr_in clientAddr;
-  socklen_t clientAddrSize = sizeof(clientAddr);
-  int clientSockfd = accept(sockfd, (struct sockaddr*)&clientAddr, &clientAddrSize);
-
-  if (clientSockfd == -1) {
-    perror("accept");
-    return 4;
-  }
-
-  char ipstr[INET_ADDRSTRLEN] = {'\0'};
-  inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
-  std::cout << "Accept a connection from: " << ipstr << ":" <<
-    ntohs(clientAddr.sin_port) << std::endl;
-
-  // read/write data from/into the connection
-  bool isEnd = false;
-  char buf[20] = {0};
-  std::stringstream ss;
-
-  while (!isEnd) {
-    memset(buf, '\0', sizeof(buf));
-
-    if (recv(clientSockfd, buf, 20, 0) == -1) {
-      perror("recv");
-      return 5;
-    }
-
-    ss << buf << std::endl;
-    std::cout << buf << std::endl;
-
-
-    if (send(clientSockfd, buf, 20, 0) == -1) {
-      perror("send");
-      return 6;
-    }
-
-    if (ss.str() == "close\n")
-      break;
-
-    ss.str("");
-  }
-
-  close(clientSockfd);
-*/
   return 0;
 }
+/*
+string HOSTNAME = "127.0.0.1";
+string PORTNUM = "4000";
+string DIRECTORY = ".";
+string HTTP_VERSION = "1.0";
+
+const size_t MAX_HTTP_MESSAGE_SIZE = 65536;
+
+void sendResponse(int clientSockfd, HTTPRequest request);
+void sendEncoded(int fd, string wire);
+
+int main(int argc, char* argv[])
+{
+    cout << "Webserver is now running" << endl;
+
+    // Check if there are a correct number of arguments with the command.
+	if (argc != 1 && argc != 4) {
+		cout << "Error Msg" << endl;
+		exit(1);
+	}
+
+    // Set the specified parameters rather than just the default ones.
+	if (argc == 4) {
+		HOSTNAME = (strcmp(argv[1], "localhost") == 0 ? "127.0.0.1" : argv[1]);
+		PORTNUM = argv[2];
+		DIRECTORY = argv[3];
+	}
+
+    // create a socket using TCP IP
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // allow others to reuse the address
+    int yes = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+    {
+        perror("setsockopt() error");
+        return 1;
+    }
+
+    // bind address to socket
+    struct sockaddr_in addr;
+    int port = atoi(PORTNUM.c_str());
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);     // short, network byte order
+    addr.sin_addr.s_addr = inet_addr(HOSTNAME.c_str());
+    memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
+    if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1)
+    {
+        perror("bind() error");
+        return 2;
+    }
+
+    // set socket to listen status
+    if (listen(sockfd, 1) == -1)
+    {
+        perror("listen() error");
+        return 3;
+    }
+
+    // read/write data from/into the connection
+    char buffer[MAX_HTTP_MESSAGE_SIZE] = {0};
+
+    while (1)
+    {
+        // accept a new connection
+        struct sockaddr_in clientAddr;
+        socklen_t clientAddrSize = sizeof(clientAddr);
+        int clientSockfd = accept(sockfd, (struct sockaddr*)&clientAddr, &clientAddrSize);
+        if (clientSockfd == -1)
+        {
+            perror("accept() error");
+            return 4;
+        }
+
+        char ipstr[INET_ADDRSTRLEN] = {'\0'};
+        inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
+
+        // Prepare 'buffer'; set '\0' to all bytes of 'buffer'.
+        memset(buffer, '\0', sizeof(buffer));
+
+        // Check for any incoming requests from web-client, and throw error if failure.
+        // When a request comes in, fill the encoded contents into buffer.
+        if (recv(clientSockfd, buffer, MAX_HTTP_MESSAGE_SIZE, 0) == -1)
+        {
+            perror("recv() error");
+            return 5;
+        }
+
+        // Create httprequest object here, and have it decode whatever is in buffer;
+        // use HTTPRequest.decode.
+        HTTPRequest request;
+        HTTPResponse response;
+
+        if (request.decode(string(buffer)) == false)
+        {
+            response.setVersion(HTTP_VERSION);
+            response.setStatusCode("400");
+            response.setStatusMessage("Bad Request");
+            sendEncoded(clientSockfd, response.encode());
+            close(clientSockfd);
+            continue;
+        }
+        // cout << "Request received!" << endl;
+        // Print out the request line.
+        cout << request.getMethod() << " " << request.getURL() << " " << request.getVersion() << endl;
+
+        // Send out a response to web-client. The stuff that will be sent it is
+        // in buffer, so I think we're supposed to either change 'buffer' to
+        // another variable or just change its contents before calling this
+        // function...
+        sendResponse(clientSockfd, request);
+
+        close(clientSockfd);
+    }
+
+    return 0;
+}
+
+string getFileContentType(string file)
+{
+	if (file.length() < 3)
+		return "";
+
+    int i;
+    for (i = file.length() - 1; i >= 0; i--) {
+        if (file[i] == '.')
+            break;
+    }
+
+    if (i == 0)
+        return "";
+
+    string fileType = file.substr(i);
+
+    if (fileType == ".html")
+        return "text/html";
+    if (fileType == ".jpg")
+        return "image/jpg";
+    if (fileType == ".jpeg")
+        return "image/jpeg";
+    if (fileType == ".gif")
+        return "image/gif";
+    if (fileType == ".png")
+        return "image/png";
+    if (fileType == ".txt")
+        return "text/plain";
+
+    return "error";
+}
+
+void sendEncoded(int fd, string wire)
+{
+    if (send(fd, wire.c_str(), wire.length(), 0) == -1)
+    {
+        perror("send() error");
+        exit(6);
+    }
+}
+
+void sendResponse(int clientSockfd, HTTPRequest request)
+{
+    // Create the HTTP Response.
+    HTTPResponse response;
+    response.setVersion(HTTP_VERSION);
+
+    // Check if URL is just "/"; if so, simply default it to "/index.html".
+    if (request.getURL() == "/")
+    {
+        request.setURL("/index.html");
+    }
+
+    string filename = DIRECTORY + request.getURL();
+    int fd = open(filename.c_str(), O_RDONLY);
+
+    // If the file was not found, prep the response for a 404 Not Found and
+    // send it.
+    if (fd == -1)
+    {
+        response.setStatusCode("404");
+        response.setStatusMessage("Not Found");
+        sendEncoded(clientSockfd, response.encode());
+        close(fd);
+        return;
+    }
+
+    // Store the contents of the file into a string.
+
+    // Get file information.
+    struct stat st;
+    if (fstat(fd, &st) == -1)
+    {
+        perror("fstat() error");
+        exit(-1);
+    }
+
+    // Get the file itself into a buffer and then into the response body.
+    char buffer[MAX_HTTP_MESSAGE_SIZE];
+    memset(buffer, '\0', sizeof(buffer));
+    if (read(fd, buffer, st.st_size) == -1)
+    {
+        perror("read() error");
+        exit(-1);
+    }
+    string bufferString = buffer;
+    response.setStatusCode("200");
+    response.setStatusMessage("OK");
+    response.setHeader("Content-length", to_string(st.st_size));
+    response.setHeader("Content-type", getFileContentType(request.getURL()));
+    response.setBody(bufferString);
+    sendEncoded(clientSockfd, response.encode());
+
+    close(fd);
+}
+*/
