56cd6a3a1b1e04a318dc85098152836b07323768
Brandon Ly <brandonly@live.com>
2016-12-04 20:17:13 -0800

    removed unnecessary code

--- simple-tcp-client.cpp
+++ simple-tcp-client.cpp
@@ -154,17 +154,6 @@ int main(int argc, char* argv[])
     ofs.write(&buffer[0], buffer.size());
     ofs.close();
 
-    // Header sendFinPacket(recvPacket.getAckNum(), (recvPacket.getSeqNum() + MSS) % MSN, MAX_RECVWIN, true, false, true);
-    // vector<char> sendFinPacketEncoded = sendFinPacket.encode();
-    // if (send(sockfd, &sendFinPacketEncoded[0], sendFinPacketEncoded.size(), 0) == -1) {
-    //      perror("send() error");
-    //      return 4;
-    // }
-    // cout << "Sending packet " << sendFinPacket.getAckNum() << " FIN" << endl;
-
-    // For FIN, do the () % modulo shit
-    // Header sendAck(received.getAckNum(), (received.getSeqNum() + 1) % MSN, 0, true, false, false);
-
     close(sockfd);
 
     return 0;
